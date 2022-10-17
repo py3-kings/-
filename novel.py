604a3f40d9b4aef21e03235e9ee967d776fbe0c3
py3-kings <110274471+py3-kings@users.noreply.github.com>
2022-10-17 10:20:18 +0800

    Update novel.py

--- novel.py
+++ novel.py
@@ -37,7 +37,7 @@ def mail(new_name):
     # 将保存好的小说发送邮箱
     data = {
         'user': '2287585490@qq.com',
-        'password': 'ydqojqrdreqfdiee',
+        'password': 'hmvcntwbmcdtecei',
         'host': 'smtp.qq.com'
     }
     yag = yagmail.SMTP(user=data['user'], password=data['password'], host=data['host'])
